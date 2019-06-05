fcdc30072fd407ec3783074d59babcda56f7b0a1
infsolution <clsinfsolution@gmail.com>
2019-06-04 21:30:11 -0300

    update aplicação horários

--- app/src/main/java/com/example/classroom/HomeHorarios.java
+++ app/src/main/java/com/example/classroom/HomeHorarios.java
@@ -3,10 +3,13 @@ package com.example.classroom;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toolbar;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -36,5 +39,28 @@ public class HomeHorarios extends AppCompatActivity {
         Intent intent = new Intent(this, HorarioList.class);
         intent.putExtra("item",item);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_horario, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_horarios) {
+            Intent intent = new Intent(this, HorarioHome.class);
+            startActivity(intent);
+            return true;
+        }
+        if (id == R.id.action_disponiveis) {
+            Intent intent = new Intent(this, HorarioDisponivel.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
